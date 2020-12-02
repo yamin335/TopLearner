96e06518dfce8babc38cfeb52bde55f584b17ce6
md.yamin <myamin335@gmail.com>
2020-12-02 12:09:33 +0600

    YAMIN -- December 2, 2020, 12:09 PM

--- app/src/main/java/com/rtchubs/engineerbooks/ui/profile_signin/ProfileSignInFragment.kt
+++ app/src/main/java/com/rtchubs/engineerbooks/ui/profile_signin/ProfileSignInFragment.kt
@@ -1,21 +1,36 @@
 package com.rtchubs.engineerbooks.ui.profile_signin
 
-import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.FileProvider
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.GenericTransitionOptions.with
+import com.bumptech.glide.Glide
+import com.darwin.viola.still.FaceDetectionListener
+import com.darwin.viola.still.Viola
+import com.darwin.viola.still.model.CropAlgorithm
+import com.darwin.viola.still.model.FaceDetectionError
+import com.darwin.viola.still.model.FaceOptions
+import com.google.android.gms.vision.Frame
+import com.google.android.gms.vision.face.FaceDetector
 import com.rtchubs.engineerbooks.BR
+import com.rtchubs.engineerbooks.BuildConfig
 import com.rtchubs.engineerbooks.R
 import com.rtchubs.engineerbooks.databinding.ProfileSignInBinding
 import com.rtchubs.engineerbooks.models.registration.City
@@ -23,12 +38,15 @@ import com.rtchubs.engineerbooks.models.registration.Gender
 import com.rtchubs.engineerbooks.models.registration.Upazilla
 import com.rtchubs.engineerbooks.ui.LoginHandlerCallback
 import com.rtchubs.engineerbooks.ui.common.BaseFragment
-import timber.log.Timber
+import com.rtchubs.engineerbooks.util.BitmapUtilss
+import com.squareup.picasso.Picasso
 import java.io.File
 import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
 
 private const val REQUEST_TAKE_PHOTO_NID_FRONT = 1
+private const val REQUEST_IMAGE_CAPTURE = 0x12345
 
 class ProfileSignInFragment : BaseFragment<ProfileSignInBinding, ProfileSignInViewModel>() {
 
@@ -59,6 +77,10 @@ class ProfileSignInFragment : BaseFragment<ProfileSignInBinding, ProfileSignInVi
     lateinit var cityAdapter: ArrayAdapter<String>
     lateinit var upazillaAdapter: ArrayAdapter<String>
     lateinit var genderAdapter: ArrayAdapter<String>
+
+    lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+    lateinit var imageCropperListener: FaceDetectionListener
+    lateinit var currentPhotoPath: String
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -97,6 +119,38 @@ class ProfileSignInFragment : BaseFragment<ProfileSignInBinding, ProfileSignInVi
         updateStatusBarBackgroundColor("#1E4356")
         registerToolbar(viewDataBinding.toolbar)
 
+        Glide.with(requireContext()).load(R.drawable.doctor_1).circleCrop().into(viewDataBinding.rivProfileImage)
+
+        imageCropperListener = object : FaceDetectionListener {
+            override fun onFaceDetected(result: com.darwin.viola.still.model.Result) {
+                val faceList = result.facePortraits
+                if (faceList.isNotEmpty()) {
+                    Glide.with(requireContext()).load(faceList[0].face).circleCrop().placeholder(R.drawable.doctor_1).into(
+                        viewDataBinding.rivProfileImage
+                    )
+                    //viewDataBinding.rivProfileImage.setImageBitmap()
+                }
+            }
+
+            override fun onFaceDetectionFailed(error: FaceDetectionError, message: String) {
+                val errorDescription = error.message
+                val tt = error.name
+            }
+        }
+
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val file = File(currentPhotoPath)
+            val imageBitmap = BitmapUtilss.getBitmapFromContentUri(requireContext().contentResolver, Uri.fromFile(file))
+
+            val viola = Viola(imageCropperListener)
+            val faceOption = FaceOptions.Builder().cropAlgorithm(CropAlgorithm.SQUARE)
+                    .enableProminentFaceDetection()
+                    .enableDebug()
+                    .build()
+            val bitmap = imageBitmap ?: return@registerForActivityResult
+            viola.detectFace(bitmap, faceOption)
+        }
+
         val temp = Array(viewModel.cities.size + 1) {""}
         temp[0] = "--Select City--"
         viewModel.cities.forEachIndexed { index, city ->
@@ -110,7 +164,12 @@ class ProfileSignInFragment : BaseFragment<ProfileSignInBinding, ProfileSignInVi
 
         viewDataBinding.spCity.onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
                 if (position != 0) {
                     try {
                         val upazillaList = viewModel.getUpazzilla(viewModel.cities[position - 1].id)
@@ -121,7 +180,11 @@ class ProfileSignInFragment : BaseFragment<ProfileSignInBinding, ProfileSignInVi
                         }
                         titleUpazillaList = tempUpazilla
 
-                        upazillaAdapter = ArrayAdapter(requireContext(), R.layout.spinner_item, titleUpazillaList)
+                        upazillaAdapter = ArrayAdapter(
+                            requireContext(),
+                            R.layout.spinner_item,
+                            titleUpazillaList
+                        )
                         upazillaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                         viewDataBinding.spUpazilla.adapter = upazillaAdapter
                     } catch (e: IndexOutOfBoundsException) {
@@ -141,7 +204,12 @@ class ProfileSignInFragment : BaseFragment<ProfileSignInBinding, ProfileSignInVi
 
         viewDataBinding.spUpazilla.onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
                 if (position != 0) {
                     try {
                         //viewModel.selectedTicketCategory.value = ticketCategoryList[position - 1]
@@ -169,7 +237,12 @@ class ProfileSignInFragment : BaseFragment<ProfileSignInBinding, ProfileSignInVi
 
         viewDataBinding.spGender.onItemSelectedListener = object :
             AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
                 if (position != 0) {
                     try {
                         //viewModel.selectedTicketCategory.value = ticketCategoryList[position - 1]
@@ -246,93 +319,106 @@ class ProfileSignInFragment : BaseFragment<ProfileSignInBinding, ProfileSignInVi
 //                }
 //            })
         }
+
         viewDataBinding.rivNidFrontImage.setOnClickListener {
             //dispatchTakePictureIntent("rivNidFrontImage")
         }
+
         viewDataBinding.rivNidBackImage.setOnClickListener {
             //dispatchTakePictureIntent("rivNidBackImage")
         }
+
+        viewDataBinding.rivProfileImage.setOnClickListener {
+            takeProfileImageFromCamera()
+        }
     }
 
-    private fun dispatchTakePictureIntent(captureFor: String) {
+//    private fun takeProfileImageFromCamera() {
+//        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//        try {
+//            cameraLauncher.launch(takePictureIntent)
+//        } catch (e: ActivityNotFoundException) {
+//            // display error state to the user
+//        }
+//    }
+
+    private fun takeProfileImageFromCamera() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
             // Ensure that there's a camera activity to handle the intent
-            takePictureIntent.resolveActivity(mContext.packageManager)?.also {
+            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
                 // Create the File where the photo should go
                 val photoFile: File? = try {
-                    createImageFile(captureFor)
+                    createImageFile()
                 } catch (ex: IOException) {
                     // Error occurred while creating the File
+                    ex.printStackTrace()
                     null
                 }
                 // Continue only if the File was successfully created
                 photoFile?.also {
                     val photoURI: Uri = FileProvider.getUriForFile(
-                        mContext,
-                        "com.rtchubs.engineerbooks.fileprovider",
+                        requireContext(),
+                        "${BuildConfig.APPLICATION_ID}.provider",
                         it
                     )
-                    Timber.d("received file uri : $photoURI")
-
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO_NID_FRONT)
+                    cameraLauncher.launch(takePictureIntent)
                 }
             }
         }
     }
 
     @Throws(IOException::class)
-    private fun createImageFile(captureFor: String): File {
+    private fun createImageFile(): File {
         // Create an image file name
-        // val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val timeStamp: String = Date().time.toString()
-        val storageDir: File = mContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
-
-        Timber.d("file directory : $storageDir")
-
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.ENGLISH).format(Date())
+        val storageDir: File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
         return File.createTempFile(
             "JPEG_${timeStamp}_", /* prefix */
             ".jpg", /* suffix */
             storageDir /* directory */
         ).apply {
             // Save a file: path for use with ACTION_VIEW intents
-            if (captureFor == "rivNidFrontImage") {
-                rivNidFrontCaptureImage = absolutePath
-            } else if (captureFor == "rivNidBackImage") {
-                rivNidBackCaptureImage = absolutePath
-            }
-            //galleryAddPic(absolutePath)
+            currentPhotoPath = absolutePath
         }
     }
 
-    // need this when file is saved in external storage public directory
-   /* private fun galleryAddPic(absolutePath: String) {
-        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
-            val f = File(absolutePath)
-            mediaScanIntent.data = Uri.fromFile(f)
-            cntx.sendBroadcast(mediaScanIntent)
+    fun getFace(context: Context, data: Bitmap): Bitmap? {
+        try {
+            var bitmap = data
+            if (bitmap.width > bitmap.height) {
+                val matrix = Matrix()
+                matrix.postRotate(270f)
+                if (bitmap.width > 1500) matrix.postScale(0.5f, 0.5f)
+                bitmap = Bitmap.createBitmap(
+                    bitmap,
+                    0,
+                    0,
+                    bitmap.width,
+                    bitmap.height,
+                    matrix,
+                    true
+                )
+            }
+            val faceDetector = FaceDetector.Builder(context).setProminentFaceOnly(true).setTrackingEnabled(
+                false
+            ).build()
+            val frame = Frame.Builder().setBitmap(bitmap).build()
+            val faces = faceDetector.detect(frame)
+            var results: Bitmap? = null
+            for (i in 0 until faces.size()) {
+                val thisFace = faces.valueAt(i)
+                val x = thisFace.position.x
+                val y = thisFace.position.y
+                val x2 = x / 4 + thisFace.width
+                val y2 = y / 4 + thisFace.height
+                results = Bitmap.createBitmap(bitmap, x.toInt(), y.toInt(), x2.toInt(), y2.toInt())
+            }
+            return results
+        } catch (e: Exception) {
+            Log.e("GET_FACE", e.message)
         }
-    }*/
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == REQUEST_TAKE_PHOTO_NID_FRONT && resultCode == Activity.RESULT_OK) {
-
-
-            //Intent data is returning empty
-
-            /*data?.data?.let { uri ->
-                viewDataBinding.rivNidFrontImage.setImageURI(uri)
-            } ?: run {
-                Timber.e("uri is null")
-            }*/
-
-           /* val imageBitmap = data?.extras?.get("data") as? Bitmap
-            viewDataBinding.rivNidFrontImage.setImageBitmap(imageBitmap)*/
-
-
-        }
+        return null
     }
 
 }
